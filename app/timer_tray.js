27a7b6e5a08ff8cb33bb1dcd83ee9729cdc55a8c
Daniel Cunha <danielbrandaodacunha@gmail.com>
2017-11-23 17:30:27 -0200

    Tray

---
+++ app/timer_tray.js
@@ -0,0 +1,48 @@
+const electron = require('electron');
+const { Tray, app, Menu } = electron;
+
+class TimerTray extends Tray {
+  constructor(iconPath, mainWindow){
+    super(iconPath);
+
+    this.mainWindow = mainWindow;
+    this.setToolTip('Timer App');
+    //inherit
+    this.on('click', this.onClick.bind(this));
+    this.on('right-click', this.onRightClick.bind(this));
+  }
+
+  onClick(event, bounds){
+    const { x, y } = bounds;
+    //window height and width
+    const { height, width } = this.mainWindow.getBounds();
+
+    if (this.mainWindow.isVisible()){
+      this.mainWindow.hide();
+    } else {
+      const yPosition = process.platform === 'darwin' ? y : (y - height) + 30;
+      console.log(yPosition);
+      this.mainWindow.setBounds({
+        x: Math.round((x - (width / 2)) - 45),
+        y: yPosition ,
+        height,
+        width
+      });
+      this.mainWindow.show();
+    }
+  }
+
+  onRightClick(){
+    const menuConfig = Menu.buildFromTemplate([
+      {
+        label: 'Quit',
+        click: () => app.quit()
+      }
+    ])
+
+    this.popUpContextMenu(menuConfig);
+  }
+
+
+}
+module.exports = TimerTray;
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+const path = require('path');
 const electron = require('electron');
+const TimerTray = require('./app/timer_tray');
 
 const { app, BrowserWindow } = electron;
 
 let mainWindow;
+let tray;
 
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
+app.on('ready', () => {  
+  mainWindow = new BrowserWindow({
+    height: 540,
+    width: 365,
+    frame: false,
+    resizable: false,
+    show: false
+  });
+
   if (process.env.NODE_ENV === 'development') {
     mainWindow.loadURL('http://localhost:4200');
   } else {
     mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
   }
+
+  mainWindow.on('blur', () =>{
+    mainWindow.hide();
+  });
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
+  tray = new TimerTray(iconPath, mainWindow);
+
 });
 
 app.on('window-all-closed', function () {
